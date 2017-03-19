2c3450ccb6861bc651cdb2e808dca1c57091df4a
Marcin <marcin.1.dabrowski@gmail.com>
2017-03-19 10:21:04 +0100

    scheduled has been set

--- src/main/java/com/md/plotter/app/cron/PlotHarvestScheduler.java
+++ src/main/java/com/md/plotter/app/cron/PlotHarvestScheduler.java
@@ -5,6 +5,7 @@ import com.md.plotter.app.repository.PlotRepository;
 import com.md.plotter.app.service.CityService;
 import com.md.plotter.app.service.PlotService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class PlotHarvestScheduler {
     @Autowired
     private PlotRepository plotRepository;
 
+    @Scheduled(cron="0 0/12 10 * * *")
     public void schedule() throws IOException {
         final List<Plot> plots = plotService.preparePlots();
         cityService.updateCities(plots);
